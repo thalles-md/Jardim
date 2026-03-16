b2da7e034ac01ec5a9ad8a0a853e7763af80aef7
sam <email@ficticio.com>
2026-03-16 15:49:29 -0300

    Agora, Postagens!

---
+++ nav.js
@@ -0,0 +1,11 @@
+document.querySelector('nav').innerHTML = `
+  <a href="/index.html" class="site-name">SAM</a>
+  <details>
+    <summary>Menu</summary>
+    <div class="menu">
+      <a href="/index.html">Home</a>
+      <a href="/escritas.html">Escritas</a>
+      <a href="/posts/posts.html">Postagens</a>
+    </div>
+  </details>
+`;
